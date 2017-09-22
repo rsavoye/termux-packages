7cd3a2866d39e6d6c2052b96a9e775f4fb944a82
Rob Savoye <rob.savoye@linaro.org>
2017-09-22 12:55:24 +0545

    Add osmctools

---
+++ packages/osmctools/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ramunasd/osmctools
+TERMUX_PKG_DESCRIPTION="OSM C Tools"
+TERMUX_PKG_SRCURL=http://www.senecass.com/termux/src/osmctools.tar.bz2
+TERMUX_PKG_VERSION=0.7
+TERMUX_PKG_BUILD_IN_SRC="yes"
+CC=gcc
+CXX=g++
